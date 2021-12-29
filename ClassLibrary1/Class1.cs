4e4e32131fb7c905058662c88828d177f48abd91
Ricardo199 <ricardo1529@gmail.com>
2021-12-29 19:37:32 +0000

    Add files via upload

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace ClassLibrary1{
+    public class Encrypt
+    {
+        public string sha1(string var)
+        {
+            byte[] temp;
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            temp = sha.ComputeHash(Encoding.UTF8.GetBytes(var));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < temp.Length; i++)
+            {
+                sb.Append(temp[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
